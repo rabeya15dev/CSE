d3db8110f1c6432731e354b44714078862280569
rabeya15dev <67573017+rabeya15dev@users.noreply.github.com>
2020-07-06 10:30:10 -0700

    Add files via upload

---
+++ my_d.c
@@ -0,0 +1,73 @@
+
+
+ /*{ char ch;
+ ch='p';
+printf("%c\n",ch);
+ch='s';
+printf("%c\n",ch);
+
+
+return 0;
+ }
+
+) /*
+{
+    float t=8.4,e=9.8;
+    printf("Addition: %f\n", t+e);
+     int u,p,s;
+     u=9;
+     p=6;
+     s=u*p;
+     printf("%d \n",'s');
+        return 0;
+
+{
+
+    printf("size of int in my computer:%d\n" ,sizeof(int));
+    return 0;
+
+}
+
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+
+    double r=4.88,t=4.55,y=777.9;
+
+    printf("value of :%.3lf\n upoma tui kharap",r*t/4);
+    return 0;
+
+
+*/
+
+#include <stdio.h>
+int main() {
+   int i,j,nila;
+   printf("nila\n: ");
+   scanf("%d", &nila);
+   for (i = 1; i <= nila; ++i) {
+      for (j = 1; nila <= i; ++j) {
+         printf("love you nila/n ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
