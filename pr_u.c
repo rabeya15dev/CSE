eddcd4bd0ac526d438586cd479e7ba397c166228
rabeya15dev <67573017+rabeya15dev@users.noreply.github.com>
2020-07-06 10:43:53 -0700

    Add files via upload

---
+++ pr_u.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int y;
+    printf("enter the value of y;\n");
+    scanf("%d",&y);
+    if( y==0)
+  {printf("hlw");
+}
+  else if(y==1)
+  {
+  printf("try try") ;
+      }
+    else {
+        printf(" Error:");
+    }
+return 0;}
+
+
+
