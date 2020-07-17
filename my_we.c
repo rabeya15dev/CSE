48155322a9ca6e979f282b47546b3e099298c076
Rabeya <67573017+rabeya15dev@users.noreply.github.com>
2020-07-17 12:23:28 -0700

    Add files via upload

---
+++ my_we.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the number\n");
+scanf("%c",&ch);
+ if( ch=='a' || ch=='e'|| ch=='i' ||ch=='o' ||ch=='u')
+
+
+{
+    printf(" vowel\n");
+
+}
+else
+{
+    printf("consonant\n");
+
+}
+
+return 0;
+
+}
