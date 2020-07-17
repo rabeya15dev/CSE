1cb27dde2d71ce2a526ecbd9097110aaec6f4dc7
Rabeya <67573017+rabeya15dev@users.noreply.github.com>
2020-07-17 12:24:04 -0700

    Add files via upload

---
+++ ran_fd.c
@@ -0,0 +1,65 @@
+
+#include<stdio.h>
+int main()
+{
+
+    float a,b;
+     printf("enter the number a and y:\n");
+    scanf("%f%f",&a ,&b);
+    printf("Answer: %f and %f");
+    return 0;
+}
+
+
+#include<stdio.h>
+int main()
+{
+
+    double s,d;
+    printf("enter the number s and d:\n");
+    scanf("%lf%lf",&s,&d);
+    printf("answer:%.6lf and %lf");
+
+
+return 0;
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    float average=(a+b+c)/3;
+    printf("enter the number a:%d\n");
+
+      printf("enter the number b:%d\n");
+        printf("enter the number c:%d\n");
+          printf("enter the number a,b,c:5.%f\n", average);
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
