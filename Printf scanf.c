2a66bbf5bd19b6967c4fe518bad45c78dd7b875d
Rabeya Bosri <67573017+rabeya15dev@users.noreply.github.com>
2021-03-14 19:52:11 +0600

    Add files via upload

---
+++ Printf scanf.c
@@ -0,0 +1,78 @@
+/*
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,sum;
+    printf("Enter the Four Number:\n");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    sum=a+b+c+d;
+    printf("The Result is: %d",sum);
+    return 0;
+
+}
+*/
+
+//2nd
+/*
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d,Sub;
+    printf("Enter the number:\n");
+    scanf("%f%f%f%f",&a,&b,&c,&d);
+    Sub=a-b-c-d;
+    printf("\t\t\t Result:%f",Sub);
+    return 0;
+}
+*/
+
+//3rd
+
+/*
+#include<stdio.h>
+int main()
+{
+    int a,b,mult;
+    printf("Enter the number:\n");
+    scanf("%d%d",&a,&b);
+    printf("result is:%d",a*b);
+    return 0;
+}
+*/
+
+//4th
+/*
+#include<stdio.h>
+int main()
+{
+    float a=30.5,b=2,c=5,div;
+    printf(" the Result:\n%f",a/b/c);
+    return 0;
+}
+*/
+
+//5
+/*
+#include<stdio.h>
+int main()
+{
+    double a,b,div;
+    printf("Enter the number:\n");
+    scanf("%lf%lf",&a,&b);
+    // or div=a\b;
+    printf("Result is:\t\t\t\t\t %8.lf",a/b);
+    return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+    char
+    return 0;
+}
+
+
+
+
+
